90fe1ea5dca65b5a26b428072a4e60469c92ee76
tprooney <46979750+tprooney@users.noreply.github.com>
2019-05-08 12:54:07 -0600

    Update DrivingButtons.py

--- DrivingButtons.py
+++ DrivingButtons.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 #seconds between buttons checking
-intervaltime = 0.5
+intervalTime = 0.5
 #radio on/off variable
 radio = 0
 #volume variable in % of maximum volume
@@ -11,13 +11,17 @@ volume = 50
 #GPIO
 ButtonIn1 = 21
 ButtonIn2 = 20
-ButtonIn3 = 26
+ButtonIn3 = 16
 ButtonIn4 = 19
 ButtonIn5 = 13
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor, GPIO.IN)
-GPIO.setup(relay, GPIO.OUT)
+GPIO.setup(ButtonIn1, GPIO.IN)
+GPIO.setup(ButtonIn2, GPIO.IN)
+GPIO.setup(ButtonIn3, GPIO.IN)
+GPIO.setup(ButtonIn4, GPIO.IN)
+GPIO.setup(ButtonIn5, GPIO.IN)
+#GPIO.setup(relay, GPIO.OUT)
 
 #activate output
 def activate(pin):
@@ -31,27 +35,36 @@ try:
     while(True):
         #Push-to-activate buttons
         if(GPIO.input(ButtonIn1)):
+            print("Button 1 pressed!")
             #TODO: Speech button
+            time.sleep(intervalTime)
         if(GPIO.input(ButtonIn2)):
+            print("Button 2 pressed!")
             #TODO: Call/Hang Up Button
+            time.sleep(intervalTime)
         if(GPIO.input(ButtonIn3)):
             #Volume up button
+            print("Button 3 pressed")
             volume = volume + 10
+            time.sleep(intervalTime)
         if(GPIO.input(ButtonIn4)):
             #Volume down button
+            print("Button 4 pressed")
             volume = volume - 10
-            
+            time.sleep(intervalTime)
         #Latch button functions
         if(GPIO.input(ButtonIn5)):
             #Radio on/off
+            print("radio on")
             radio = 1
+            time.sleep(intervalTime)
         else:
             radio = 0
         
         #LOGIC FOR FUNCTIONALITY GOES HERE
         
         #delay between update checks
-        time.sleep(intervalTime)
+        #time.sleep(intervalTime)
 #Allow the program to exit at any time
 except KeyboardInterrupt:
     print("Quiting program and cleaning up.")
